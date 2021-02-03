38ee939ca1d0f0f3bb48e74e6550f107473adaa8
JAKE LEE <lee@JAKEs-MacBook-Pro.local>
2021-02-03 07:08:13 -0500

    Initial commit - starter file

---
+++ gridsome.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteName: 'Lanescape',
+  plugins: []
+}
---
+++ gridsome.server.js
@@ -0,0 +1,5 @@
+module.exports = function(api) {
+  api.loadSource(({ addCollection }) => {})
+
+  api.createPages(({ createPage }) => {})
+}
